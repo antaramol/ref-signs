c0fb09d1e150a7df85dfab552d557e87fb3f7634
antonio <antaramol@alum.us.es>
2023-01-12 14:12:42 +0100

    implementacion raspberry con tensorflow lite

--- inferencia.py
+++ inferencia.py
@@ -17,12 +17,23 @@ t_ant=t_ini
 muestras=28 # muestras que tomamos, 1 min son 1600
 accel_average=[]
 
-import keras
-from keras.models import Sequential
+import tflite_runtime.interpreter as tflite
 
-model_inf = keras.models.load_model("mejor_modelo.h5")
+
+# Initialize the interpreter
+interpreter = tflite.Interpreter(model_path="model.tflite")
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()[0]
+output_details = interpreter.get_output_details()[0]
+
+print("No es el modelo cuantizado:")
+print('input: ', input_details['dtype'])
+print('output: ', output_details['dtype'])
+
 
 labels = ['anular','ataque','pasos','tecnica']
+prediction = np.zeros(len(labels),dtype=int)
 
 
 archivo="ataque" # nombre del archivo
@@ -32,7 +43,7 @@ print("grabando")
 #bucle for para tomar los datos
 while(1):
     for i in range(muestras):
-        print(i)
+        #print(i)
         #t_actual=time.time()
         acceleration = sense.get_accelerometer_raw()
         #datos de aceleración en Gs
@@ -50,11 +61,24 @@ while(1):
         
         #t_ant=t_actual
 
-    print("Todas las muestras tomadas")
+    print("Tenemos 2 segundos muestreados")
 
     df = pd.DataFrame({'accel_x': accel_x, 'accel_y': accel_y, 'accel_z': accel_z, 'gyros_x': gyros_x, 'gyros_y': gyros_y, 'gyros_z': gyros_z})
-    print(df)
-    x_test = df.values.reshape(1, 28, 6)
-    repuesta = np.argmax(model_inf.predict(x_test))
-    print(labels[repuesta])
+    #print(df)
+    x_test = df.values.reshape(1, 28, 6).astype(input_details["dtype"])
+    #x_test = np.expand_dims(x_test, axis=0).astype(input_details["dtype"])
+    interpreter.set_tensor(input_details["index"], x_test)
+    interpreter.invoke()
+    output = interpreter.get_tensor(output_details["index"])[0]
+    print(output)
+    prediction = output.argmax()
+    print(labels[prediction])
+    
+    accel_x.clear()
+    accel_y.clear()
+    accel_z.clear()
+    
+    gyros_x.clear()
+    gyros_y.clear()
+    gyros_z.clear()    
 
